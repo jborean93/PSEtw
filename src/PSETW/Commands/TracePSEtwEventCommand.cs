745b716038745f7aad5c1732be1d43bd12172189
Jordan Borean <jborean93@gmail.com>
2023-08-09 11:28:36 +0000

    Added Trace-PSETWEvent command

---
+++ src/PSETW/Commands/TracePSEtwEventCommand.cs
@@ -0,0 +1,109 @@
+using System;
+using System.Collections.Concurrent;
+using System.Diagnostics;
+using System.Management.Automation;
+using System.Threading;
+using PSETW.Native;
+
+namespace PSETW.Commands;
+
+[Cmdlet(VerbsDiagnostic.Trace, "PSEtwEvent", DefaultParameterSetName = "Single")]
+public sealed class TracePSEtwEventCommand : PSCmdlet, IDisposable
+{
+    private BlockingCollection<ETWEventArgs> _events = new();
+
+    [Parameter]
+    [ArgumentCompleter(typeof(SessionNameCompletor))]
+    public string? SessionName { get; set; }
+
+    [Parameter(
+        Mandatory = true,
+        ParameterSetName = "Single"
+    )]
+    [ArgumentCompleter(typeof(ProviderCompleter))]
+    public ProviderStringOrGuid? Provider { get; set; }
+
+    [Parameter(
+        ParameterSetName = "Single"
+    )]
+    [ArgumentCompleter(typeof(KeywordCompleter))]
+    public KeywordsStringOrLong[] KeywordsAny { get; set; } = Array.Empty<KeywordsStringOrLong>();
+
+    [Parameter(
+        ParameterSetName = "Single"
+    )]
+    [ArgumentCompleter(typeof(KeywordCompleter))]
+    public KeywordsStringOrLong[] KeywordsAll { get; set; } = Array.Empty<KeywordsStringOrLong>();
+
+    [Parameter(
+        ParameterSetName = "Single"
+    )]
+    [ArgumentCompleter(typeof(LevelCompletor))]
+    public LevelStringOrInt[] Level { get; set; } = Array.Empty<LevelStringOrInt>();
+
+    protected override void ProcessRecord()
+    {
+        Debug.Assert(Provider != null);
+        Guid providerGuid = Provider!.GetProviderGuid();
+
+        FieldInfo[] keywords = ProviderHelper.GetProviderFieldInfo(providerGuid,
+            EventFieldType.EventKeywordInformation);
+        long keywordsAny = 0;
+        foreach (KeywordsStringOrLong kwd in KeywordsAny)
+        {
+            keywordsAny |= kwd.GetKeywordLong(keywords);
+        }
+        long keywordsAll = 0;
+        foreach (KeywordsStringOrLong kwd in KeywordsAll)
+        {
+            keywordsAll |= kwd.GetKeywordLong(keywords);
+        }
+
+        FieldInfo[] levels = ProviderHelper.GetProviderFieldInfo(providerGuid,
+            EventFieldType.EventLevelInformation);
+        int level = 0;
+        foreach (LevelStringOrInt lvl in Level)
+        {
+            level |= lvl.GetLevelInt(levels);
+        }
+
+        TraceSession session;
+        if (string.IsNullOrEmpty(SessionName))
+        {
+            session = PSETWGlobals.DefaultETWSession;
+        }
+        else
+        {
+            session = TraceSession.Open(SessionName!);
+        }
+
+        session.EnableTrace(
+            providerGuid,
+            (int)EventControlCode.EVENT_CONTROL_CODE_ENABLE_PROVIDER,
+            (byte)level,
+            keywordsAny,
+            keywordsAll);
+
+        using Trace trace = session.OpenTrace();
+        trace.EventReceived += EventReceived;
+        trace.Start();
+
+        foreach (ETWEventArgs args in _events.GetConsumingEnumerable())
+        {
+            WriteObject(args.Message);
+        }
+    }
+
+    private void EventReceived(object? sender, ETWEventArgs args)
+        => _events.Add(args);
+
+    protected override void StopProcessing()
+    {
+        _events?.CompleteAdding();
+    }
+
+    public void Dispose()
+    {
+        _events?.Dispose();
+    }
+}
