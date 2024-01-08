66d15a1369a9323248565e4cb25114b115a4d1b3
Jordan Borean <jborean93@gmail.com>
2024-01-09 05:29:00 +1000

    Update ModuleFast to v0.1.0
    Updates the version of ModuleFast to v0.1.0 to track the latest changes
    and move off the older version.

--- tools/common.ps1
+++ tools/common.ps1
@@ -164,34 +164,13 @@ Function Assert-ModuleFast {
         [string]$Version = 'latest'
     )
 
-    $versionPath = if ($Version -eq 'latest') {
-        'latest/download'
-    }
-    else {
-        "download/$Version"
-    }
-
-    $ModuleName = 'ModuleFast'
-    $Uri = "https://github.com/JustinGrote/$ModuleName/releases/$versionPath/$ModuleName.psm1"
-
-    if (Get-Module $ModuleName) {
-        Write-Warning "Module $ModuleName already loaded, skipping bootstrap."
+    $moduleName = 'ModuleFast'
+    if (Get-Module $moduleName) {
+        Write-Warning "Module $moduleName already loaded, skipping bootstrap."
         return
     }
 
-    try {
-        $httpClient = [HttpClient]::new()
-        $httpClient.DefaultRequestHeaders.AcceptEncoding.Add('gzip')
-        $response = $httpClient.GetStringAsync($Uri).GetAwaiter().GetResult()
-    }
-    catch {
-        $PSItem.ErrorDetails = "Failed to fetch $ModuleName from $Uri`: $PSItem"
-        $PSCmdlet.ThrowTerminatingError($PSItem)
-    }
-
-    $scriptBlock = [ScriptBlock]::Create($response)
-
-    New-Module -Name $ModuleName -ScriptBlock $scriptblock | Import-Module
+    & ([scriptblock]::Create((Invoke-WebRequest -Uri 'bit.ly/modulefast'))) -Release $Version
 }
 
 Function Assert-PowerShell {
@@ -405,7 +384,7 @@ Function Install-BuildDependencies {
             return
         }
 
-        Assert-ModuleFast -Version v0.0.1
+        Assert-ModuleFast -Version v0.1.0
 
         $installParams = @{
             ModulesToInstall = $modules
